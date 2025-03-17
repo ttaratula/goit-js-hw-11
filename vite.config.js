81a44aaf79148508a2d531262cac76119cf21fbb
Tetiana Taratula <ttaratula@gmail.com>
2025-03-17 18:33:27 +0000

    www

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig(({ command }) => {
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: glob.sync('./src/public/index.html'),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
